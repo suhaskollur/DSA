566fd8123958c944579473c90d463a3a3ed3740f
Suhas Kollur <suhas720@gmail.com>
2025-02-01 20:31:09 -0500

    few more problems on Binary Trees, moving towards the medium problems

---
+++ binaryTree/iterativeInorder.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+
+public:
+    vector<int> iterativeInOrder(TreeNode* root) {
+        vector<int> inorder;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while(!st.empty()) {
+            if(node != nullptr) {
+                st.push(node);
+                node = node -> left;
+            } else {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+                inorder.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        return inorder;    
+    }
+};
